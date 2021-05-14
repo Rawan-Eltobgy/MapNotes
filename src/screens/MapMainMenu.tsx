93576c38c9a7ae99b09885cf77d71de24d4230cc
Rawan Eltobgy <rawan.eltobgy@harmonica.life>
2021-05-14 05:49:08 +0200

    Enabling map to get user location

--- src/screens/MapMainMenu.tsx
+++ src/screens/MapMainMenu.tsx
@@ -2,14 +2,40 @@ import React, { useState } from 'react'
 import { View,Text, StyleSheet } from 'react-native'
 import MapboxGL from "@react-native-mapbox-gl/maps";
 
-MapboxGL.setAccessToken("");
+MapboxGL.setAccessToken("sk.eyJ1Ijoicm93YW54eXoiLCJhIjoiY2tvbmMwd2J2MDAwNDJ2cGl2OTJseG5ndyJ9.B0D-CDqQF-lXGZmAaUpEiQ");
 
 export default function MapMainMenu() {
+    const [latitude, setLatitude] =useState(0)
+    const [longitude, setLongitude] =useState(0)
+    const onUserLocationUpdate = async (location: MapboxGL.Location) => {
+        console.log(location)
+        let lat = location.coords.latitude;
+        let long = location.coords.longitude;
+        let alt = location.coords.altitude;
+        setLatitude(lat)
+        setLongitude(long)
+    } 
+
     return (
-        <View style={styles.page}>
-        <View style={styles.container}>
-          <MapboxGL.MapView style={styles.map} />
-        </View>
+        <View style={{flex: 1, height: "100%", width: "100%" }}>
+        <MapboxGL.MapView
+          styleURL={MapboxGL.StyleURL.Street}
+          zoomLevel={13}
+          centerCoordinate={[latitude, longitude]}
+          showUserLocation={true}
+          style={{flex: 1}}>
+            <MapboxGL.Camera
+              zoomLevel={13}
+              centerCoordinate={[latitude, longitude]}
+              animationMode={'flyTo'}
+              animationDuration={0}            >
+            </MapboxGL.Camera>
+            <MapboxGL.UserLocation
+                   visible={true}
+                onUpdate={(loc) => onUserLocationUpdate(loc)}
+            />
+
+        </MapboxGL.MapView>
       </View>
     )
 }
