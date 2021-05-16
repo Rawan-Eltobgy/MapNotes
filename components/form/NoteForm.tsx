6d5ee94ab1b0c156ae2ad821a574dadbc641e98e
Rawan Eltobgy <rawan.eltobgy@harmonica.life>
2021-05-16 14:55:24 +0200

    Initial webpack setup for react-native-web

--- components/form/NoteForm.tsx
+++ components/form/NoteForm.tsx
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
         width: '50%',
         marginTop: '8%',
         height: 40,
-        borderRadius: 10,
+        borderRadius: 8,
         borderColor: Colors.gray,
         borderWidth: 1,
         alignItems: 'center',
